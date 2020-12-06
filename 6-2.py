3c5130d586c933aafda4e0351d7be0eddab28bab
David Kiger <david.kiger@gmail.com>
2020-12-05 21:46:29 -0800

    Finished day 6.

---
+++ 6-2.py
@@ -0,0 +1,10 @@
+data = [[y for y in x.strip().split('\n')] for x in open('6-1.in').read().split('\n\n')]
+
+total = 0
+for f in data:
+    s = set(f[0])
+    for x in f[1:]:
+        s &= set(x)
+    total += len(s)
+
+print(total)
